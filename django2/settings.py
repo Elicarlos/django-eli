8bad91e6cb168950aa5afbf3690c07afad5009cd
Elicarlos <elicarlosantos_@hotmail.com>
2021-10-18 00:20:22 -0300

    ajuste em nome do projeto

--- django2/settings.py
+++ django2/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware', # adicionado para servir staticos
+    'whitenoise.middleware.WhiteNoiseMiddleware', # adicionado para servir staticos
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
